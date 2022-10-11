b6ff180e0ab3e0158f1bfacfd894416c8c694a76
Deonna <thorntondc1@gmail.com>
2022-10-11 12:48:55 -0500

    added borders to blocks and a dictionary

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ BLACK = (0,0,0)
 GRAY = (110,110,110)
 GREEN = (0,255,0)
 WHITE = (255,255,255)
+BLUE = (0, 0, 255)
 
 
 #Create blocks 
@@ -32,7 +33,7 @@ BLOCK11 = ['L','R','R','D','G','G']
 BLOCK12= ['W','P','T','H','H','T']
 
 #Used to hold the "block" letters 
-LETTER_list =  []
+LETTER_LIST =  []
 
 
 BLOCK_LIST=[BLOCK1,BLOCK2,BLOCK3,BLOCK4,BLOCK5,BLOCK6,
@@ -46,7 +47,7 @@ pygame.init()
 
 def genBlocks():
         for block in BLOCK_LIST:
-            LETTER_list.append(random.choice(block))
+            LETTER_LIST.append(random.choice(block))
             
 
 genBlocks()
@@ -58,44 +59,29 @@ pygame.display.set_caption("Q-LESS")
 
 
 
-IMG1= pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[0]))),(100,100))
-LETTER1 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[0]))),(50,50))
+LETTER1= pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[0]))),(50,50))
 
-IMG2= pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[1]))),(100,100))
-LETTER2 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[1]))),(50,50))
+LETTER2= pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[1]))),(50,50))
 
-IMG3 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[2]))),(100,100))
-LETTER3 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[2]))),(50,50))
+LETTER3 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[2]))),(50,50))
 
-IMG4 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[3]))),(100,100))
-LETTER4 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[3]))),(50,50))
+LETTER4 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[3]))),(50,50))
 
-IMG5 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[4]))),(100,100))
-LETTER5 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[4]))),(50,50))
+LETTER5 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[4]))),(50,50))
 
-IMG6 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[5]))),(100,100))
-LETTER6 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[5]))),(50,50))
+LETTER6 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[5]))),(50,50))
 
-IMG7 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[6]))),(100,100))
-LETTER7 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[6]))),(50,50))
+LETTER7 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[6]))),(50,50))
 
-LETTER8 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[7]))),(50,50))
-IMG8 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[7]))),(100,100))
+LETTER8 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[7]))),(50,50))
 
-LETTER9 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[8]))),(50,50))
-IMG9 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[8]))),(100,100))
+LETTER9 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[8]))),(50,50))
 
-LETTER10 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[9]))),(50,50))
-IMG10 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[9]))),(100,100))
+LETTER10 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[9]))),(50,50))
 
-LETTER11 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[10]))),(50,50))
-IMG11 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[10]))),(100,100))
+LETTER11 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[10]))),(50,50))
 
-LETTER12 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[11]))),(50,50))
-IMG12 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_list[11]))),(100,100))
-
-TEST_DICT = {}
-#TEST_LIST = [LETTER1,LETTER2]
+LETTER12 = pygame.transform.scale(pygame.image.load(os.path.join('images','{}.png'.format(LETTER_LIST[11]))),(50,50))
 
 
 class DragOperator:
@@ -103,17 +89,19 @@ class DragOperator:
         self.rect = rect
         self.dragging = False
         self.rel_pos = (0,0)
-
+        
     def collisioncheck(self):
         check_group = pygame.sprite.Group([letter for letter in group if letter != self.rect])
         if pygame.sprite.spritecollide(self, check_group, False):
+#            pygame.draw.rect(self, GREEN, self.rect,6)
             print("test")
-            print()
-    
-    def update(self, event_list):
+      
+
+
+
+    def movementupdate(self, event_list):
         DragOperator.collisioncheck(self)
         for event in event_list:
-        
             if event.type == pygame.MOUSEBUTTONDOWN:
                 self.dragging = self.rect.collidepoint(event.pos)
                 self.rel_pos = event.pos[0] - self.rect.x, event.pos[1] - self.rect.y
@@ -133,19 +121,19 @@ class SpriteObject(pygame.sprite.Sprite):
         self.y = y
         super().__init__() 
         
-        
 
         self.original_image = img
-        pygame.draw.rect(self.original_image, (0,0,0), (100, 100, 100, 100))
+        pygame.draw.rect(self.original_image, BLUE, self.original_image.get_rect(),3)
         self.drag_image = pygame.Surface((300, 300), pygame.SRCALPHA)
         self.image = self.original_image 
         self.rect = self.original_image.get_rect(center = (x, y))
-        self.drag = DragOperator(self.rect)
 
+        self.drag = DragOperator(self.rect)
+    
        
     def update(self, event_list):
-        self.drag.update(event_list) 
-        self.image = self.image if self.drag.dragging else self.original_image
+        self.drag.movementupdate(event_list) 
+        #self.image = self.image if self.drag.dragging else self.original_image
 
 
 
@@ -155,7 +143,7 @@ group = pygame.sprite.Group([
 
     SpriteObject(80, 500, LETTER1),
     SpriteObject(140,500, LETTER2),
-    SpriteObject(200,500,LETTER3),
+    SpriteObject(200,500, LETTER3),
     SpriteObject(260,500, LETTER4),
     SpriteObject(320,500, LETTER5),
     SpriteObject(380,500, LETTER6),
@@ -168,26 +156,23 @@ group = pygame.sprite.Group([
 ])
 
 
+TEST_DICT = {}
 
+for key, value in zip(group,LETTER_LIST):
+    TEST_DICT[key] = value
+
+
+print(TEST_DICT)
 
 def main():
     running = True
     moving = False
     FPS = 60
     GAMEFONT = pygame.font.SysFont('comicsans',25)
-    print(LETTER_list)
+    print(LETTER_LIST)
     
     clock = pygame.time.Clock()
-
-    #could potentially put this in a list to create a for loop below under redraw_window 
-    #L1 = Block(300,400,IMG1)
     
-    
-    #L2 = Block(200,400,IMG2)
-    
-    
-    
-
     
     def redraw_window():
         SCREEN.fill(WHITE)
@@ -201,7 +186,7 @@ def main():
         #L2.draw(SCREEN)
         #Draw Text 
         heading_surface = GAMEFONT.render("Letters",1,(0,0,0))
-        letter_surface = GAMEFONT.render(', '.join(LETTER_list),1,(0,0,0))
+        letter_surface = GAMEFONT.render(', '.join(LETTER_LIST),1,(0,0,0))
         SCREEN.blit(heading_surface,(300,50))
         #SCREEN.blit(letter_surface,(170,90))
        
@@ -209,9 +194,6 @@ def main():
     
     while running:
         
-
-
-
         clock.tick(FPS)
         #redraw_window()
         event_list = pygame.event.get()
